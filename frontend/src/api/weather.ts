c552277d2a27916535a53402e4be56241a95fb38
Filip Sergiu Pascuti <filip.pascuti@sectorlabs.ro>
2025-07-26 02:21:55 +0300

    Add Tanstack Query
    - added tanstack query and query options for corresponding endpoints

--- frontend/src/api/weather.ts
+++ frontend/src/api/weather.ts
@@ -1,12 +1,11 @@
-import { ForecastResponse, SearchLocationResponse } from '@customTypes/api/weather';
+import { ForecastResponse, SearchLocationResponse, Location } from '@customTypes/api/weather';
+import { queryOptions } from '@tanstack/react-query';
 
 const WEATHER_API_SEARCH_URL = 'https://api.weatherapi.com/v1/search.json';
 const WEATHER_API_FORECAST_URL = 'https://api.weatherapi.com/v1/forecast.json';
 const NUMBER_OF_FORECAST_DAYS = 7;
 
-export const getLocationAutocompleteValues = async (
-    query: string,
-): Promise<SearchLocationResponse[]> => {
+const getLocationAutocompleteValues = async (query: string): Promise<SearchLocationResponse[]> => {
     if (!query.trim()) return [];
 
     const url = new URL(WEATHER_API_SEARCH_URL);
@@ -22,7 +21,15 @@ export const getLocationAutocompleteValues = async (
     return await response.json();
 };
 
-export const locationForecast = async (location?: string): Promise<ForecastResponse> => {
+export const locationAutocompleteValuesOptions = (inputValue: string) =>
+    queryOptions({
+        queryKey: ['cities', inputValue],
+        queryFn: () => getLocationAutocompleteValues(inputValue),
+        enabled: inputValue.length > 0,
+        staleTime: 10 * 1000,
+    });
+
+const locationForecast = async (location?: string): Promise<ForecastResponse> => {
     if (!location || !location.trim()) {
         throw new Error('Location is required to fetch the forecast.');
     }
@@ -42,3 +49,11 @@ export const locationForecast = async (location?: string): Promise<ForecastRespo
 
     return (await response.json()) as ForecastResponse;
 };
+
+export const locationForecastOptions = (location: Location | null) =>
+    queryOptions({
+        queryKey: ['forecast', location?.name],
+        queryFn: () => locationForecast(location?.name),
+        enabled: !!location?.name,
+        staleTime: 60 * 60 * 1000,
+    });
